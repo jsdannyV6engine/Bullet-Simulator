43137d817d14000cb16ac5e861a1c633eb99475a
Danny <73673422+jsdannyV6engine@users.noreply.github.com>
2020-11-27 15:06:57 -0600

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,38 @@
+var wall, thickness;
+var bullet, speed, weight;
+
+function setup() {
+  createCanvas(1600,400);
+
+speed=random(55,90)
+weight=random(400,1500)
+thickness=random(22,83)
+
+bullet=createSprite(50,200,50,20);
+bullet.velocityX = speed;
+bullet.shapeColor=color(209,197,0)
+
+wall=createSprite(1200, 200, thickness, height / 2)
+wall.shapeColor = "white";
+
+bullet.collide(wall);
+}
+
+function draw() {
+  background("black");  
+  drawSprites();
+  if(wall.x-bullet.x < (bullet.width+wall.width)/2)
+{
+  bullet.velocityX=0;
+  var deformation=0.5 * weight * speed* speed/22509;
+  if(deformation>100)
+  {
+    bullet.shapeColor=color(255,0,0);
+  }
+
+  if(deformation<180)
+  {
+    bullet.shapeColor=color(0,255,0);
+  }
+}
+}
